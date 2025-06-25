e41654c460e8b340f848d1724bd9aeea2a927b8f
bchen17 <bchen17@binghamton.edu>
2025-06-24 21:37:39 -0600

    initial changes

---
+++ tickers/get_tickers_nasdaq_fetch.py
@@ -0,0 +1,15 @@
+import pandas as pandas
+import os
+import datetime as dt 
+import yfinance as yf
+import requests
+
+
+def fetch_nasdaq_tickers(save_path="ticker_list.txt"):
+    url_nasdaq = "https://ftp.nasdaqtrader.com/dynamic/SymDir/nasdaqlisted.txt"
+    url_other = "https://ftp.nasdaqtrader.com/dynamic/SymDir/otherlisted.txt"
+    response = requests.get(url_nasdaq)
+    with open("nasdaqlisted.txt", "w") as f:
+        f.write(response.text)
+
+fetch_nasdaq_tickers()
